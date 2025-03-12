3169836ba09b541a0e80ced12ad5763ca8dc7395
Sandhya <sandhyagh20cl@gmail.com>
2025-03-12 12:09:49 +0530

    P4 Added

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace P4_Async
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            StatusLabel.Content = "Processing order... ";
+            await ProcessOrder();
+
+            StatusLabel.Content = $"Your order for '{OrderName.Text}' is ready!";
+            OrderName.Clear();
+        }
+
+        private async Task ProcessOrder()
+        {
+            await Task.Delay(3000);
+        }
+    }
+}
